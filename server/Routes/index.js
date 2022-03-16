732e74459831d797ead575df84e87a9032c2702e
Shervin <khanim97@gmail.com>
2022-03-16 15:48:01 +0330

    1

--- server/Routes/index.js
+++ server/Routes/index.js
@@ -2,19 +2,28 @@ var express = require("express")
 var router = express.Router() 
 
 
+
+// -----------------------------------------------------
+// Functions
+// -----------------------------------------------------
+function Function1(request, response){
+    response.send("<h1> Hello World from Route (1) </h1>")
+}
+
+function Function2(request, response){
+    response.send("<h1> Hello World from Route (2) </h1>")
+}
+
+
 // -----------------------------------------------------
 // All Routes
 // -----------------------------------------------------
 
 // Route (1)
-router.get("/", function(request, response){
-    response.send("<h1> Hello World from Route (1) </h1>")
-}); 
+router.get("/", Function1); 
 
 // Route (2)
-router.get("/anything", function(request, response){
-    response.send("<h1> Hello World from Route (2) </h1>")
-});       
+router.get("/anything", Function2);       
 
 // -----------------------------------------------------
 // export
